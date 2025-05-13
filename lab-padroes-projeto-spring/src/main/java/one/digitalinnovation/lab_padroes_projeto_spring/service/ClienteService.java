e302529e86985aaf76e71a871effa21cb5d5b8c4
Joao <vidigaljoaogabriel@gmail.com>
2025-05-13 16:25:40 -0300

    padroes em projetos

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/service/ClienteService.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.lab_padroes_projeto_spring.service;
+
+import one.digitalinnovation.lab_padroes_projeto_spring.model.Cliente;
+
+
+public class ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
